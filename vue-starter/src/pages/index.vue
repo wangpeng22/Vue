7a7760d1fad77197e00714c1943ad714c36892e6
wangpeng22 <852957631@qq.com>
2017-09-22 16:03:50 +0800

    update

---
+++ vue-starter/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>例子目录：</h2>
+		 <ol>
+      		<li>父->子 组件通讯：props <router-link to="props" >点击进入</router-link></li>
+		</ol>
+	</div>
+</template>
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+ /* display: inline-block;*/
+  margin: 0 10px;
+}
+</style>
